c2c87227f2991545c239c22ba6f07b4303031859
kuzeyiyidiker <kuzeyefeiyidiker@gmail.com>
2021-06-09 03:55:37 +0300

    article summary and article list components added

---
+++ src/app/article-summary/article-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-summary',
+  templateUrl: './article-summary.component.html',
+  styleUrls: ['./article-summary.component.scss']
+})
+export class ArticleSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
